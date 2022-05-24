470c86f433051cc81f71669ea095d488e61359f8
Christy Lam <cyyllam@gmail.com>
2022-05-24 14:00:19 -0700

    populate selectInput with selections

--- server.R
+++ server.R
@@ -1,17 +1,14 @@
 # Define server logic required
 server <- function(input, output, session) {
   
-  rund <- "/media/aws-prod-file01modeldata/vision2050/opusgit/urbansim_data/data/psrc_parcel/runs"
+  # rund <- "/media/aws-prod-file01modeldata/vision2050/opusgit/urbansim_data/data/psrc_parcel/runs"
   # rund2 <- "/media/aws-prod-file01modeldata2/vision2050/opusgit/urbansim_data/data/psrc_parcel/runs"
-  # 'L:/vision2050/opusgit/urbansim_data/data/psrc_parcel/runs'
+  local <- 'L:/vision2050/opusgit/urbansim_data/data/psrc_parcel/runs'
   
-  volumes <- c(Home = rund, "R Installation" = R.home(), getVolumes()())
+  volumes <- c(Home = local, "R Installation" = R.home(), getVolumes()())
   # volumes <- c(Home = fs::path_home(), "R Installation" = R.home(), getVolumes()())
   # by setting `allowDirCreate = FALSE` a user will not be able to create a new directory
   shinyDirChoose(input, "directory", roots = volumes, session = session, restrictions = system.file(package = "base"), allowDirCreate = FALSE)
-  
-  # shinyFileChoose(input, "file", roots = volumes, session = session)
-  # shinyFileSave(input, "save", roots = volumes, session = session, restrictions = system.file(package = "base"))
   
   ## print to console to see how the value of the shinyFiles 
   ## button changes after clicking and selection
@@ -19,15 +16,7 @@ server <- function(input, output, session) {
   observe({
     cat("\ninput$directory value:\n\n")
     print(input$directory)
-  })
-  
-  ## print to browser
-  output$filepaths <- renderPrint({
-    if (is.integer(input$file)) {
-      cat("No files have been selected (shinyFileChoose)")
-    } else {
-      parseFilePaths(volumes, input$file)
-    }
+    print(input$directory)
   })
   
   output$directorypath <- renderPrint({
@@ -38,4 +27,24 @@ server <- function(input, output, session) {
     }
   })
   
+  # -------------------------------------------------------
+  
+  r <- reactiveValues(runs = c())
+  
+  observeEvent(input$directory, {
+    dir.path <- parseDirPath(volumes, input$directory)
+    run.name <- str_extract(dir.path, "(?<=/)run_.*$")
+    names(dir.path) <- run.name
+    r$runs <- c(r$runs, dir.path)
+  })
+  
+  output$widget <- renderUI({
+    if(length(r$runs > 0)) {
+      selectInput('allRuns',
+                  label = 'Run Choices',
+                  selected = r$runs,
+                  choices = r$runs,
+                  multiple = TRUE)
+    }
+  })
 }
--- ui.R
+++ ui.R
@@ -1,32 +1,13 @@
-ui <- fluidPage(
-  
-  fluidPage(
-    # theme = bslib::bs_theme(version = 4),
-    headerPanel(
-      "Selections with shinyFiles",
-      "shinyFiles example"
-    ),
-    sidebarLayout(
-      sidebarPanel(
-        shinyDirButton("directory", "Folder select", "Please select a folder"),
-        tags$p(),
-        tags$p("This button lets the user navigate the file system and select a\n
-             folder. The absolute path of the selected folder is then sent\n
-             back to the server. While only folders can be selected, it is\n
-             possible to get an overview of the content beforehand. \n
-             Furthermore it is permission aware and warns if a folder with \n
-             missing write permissions is selected. Lastly it is possible to\n
-             create folders on the fly")
-        
-      ),
-      mainPanel(
-        tags$h4("The output of a folder selection"),
-        tags$p(HTML("When a folder is selected the position of the folder is sent to \n
-                  the server and can be formatted with <code>parseDirPath()</code> to reflect a\n
-                  standard path string as returned by e.g. <code>choose.dir()</code> on windows\n
-                  systems.")),
-        verbatimTextOutput("directorypath")
-      )
-    )
+
+fluidPage(
+  headerPanel(
+    "Selections with shinyFiles",
+    "shinyFiles example"
+  ),
+  mainPanel(
+    shinyDirButton("directory", "Folder select", "Please select a folder"),
+    verbatimTextOutput("directorypath"),
+    uiOutput('widget')
   )
 )
+
